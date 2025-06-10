63edf54e0f5ce5371c9b94e7a65fa552c6430e7a
c0a242327c <c0a242327c@edu.teu.ac.jp>
2025-06-10 16:00:15 +0900

    練習9:画像残像問題解決

--- pygame_image.py
+++ pygame_image.py
@@ -20,9 +20,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img2, [-tmr+1600, 0])
+        x = tmr
+        x = tmr % 3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, [300, 200])
+        
         pg.display.update()
         tmr += 1        
         clock.tick(200)
